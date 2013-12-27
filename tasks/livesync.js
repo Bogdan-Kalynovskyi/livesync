9e5a319e080e64162d28f07af33b128fe39548e7
easy-one <smoking22>
2013-12-27 12:14:07 +0100

    todo promt remove all

--- tasks/livesync.js
+++ tasks/livesync.js
@@ -48,7 +48,17 @@ module.exports = function (grunt) {
         try {
             if (options.initialRemove) {
                 console.log('Removing ' + target + ' dir...');
-                console.log('PROMT REMOVE ALL');
+
+                //todo
+                /*var yesno = require('yesno');
+
+                 yesno.ask('Are you sure you want to continue?', true, function(ok) {
+                 if(ok) {
+                 console.log("Yay!");
+                 } else {
+                 console.log("Nope.");
+                 }
+                 });*/
                 removeDirSync(target);
             }
             if (options.initialCopy) {
